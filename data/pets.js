a0ef05b71bf3fed55c0f526fcfc513b8bb02d4ca
buaa-yaoqiu <2358570788@qq.com>
2026-09-14 21:41:40 +0800

    Maintain pet catalog and wiki evolution chains

--- data/pets.js
+++ data/pets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a9e31b7dd62a0e30c8df4c59506f49052e137cccfaaf0352def73c9d07f7595
-size 119568
+oid sha256:f6193f61e93bd1e574cc644ede221d2c65cf07924c5bae32205380b577dd9912
+size 121163
